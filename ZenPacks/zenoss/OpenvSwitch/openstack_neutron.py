062ee50eac2d6b2081442b26667532088e2e138b
Philip Carinhas <pcarinhas@zenoss.com>
2016-02-01 13:01:00 -0600

    Fixes ZEN-21744
    * add check for openstack_neutron_integration

--- ZenPacks/zenoss/OpenvSwitch/openstack_neutron.py
+++ ZenPacks/zenoss/OpenvSwitch/openstack_neutron.py
@@ -17,55 +17,48 @@ from zope.container.interfaces import IObjectRemovedEvent
 from Products.Zuul.catalog.events import IndexingEvent
 from Products.Zuul.interfaces import ICatalogTool
 
-try:
-    from ZenPacks.zenoss.OpenStackInfrastructure.interfaces \
-        import INeutronImplementationPlugin
-    from ZenPacks.zenoss.OpenStackInfrastructure.neutron_integration \
-        import BaseNeutronImplementationPlugin, split_list, reindex_core_components
-    openstack = True
-except ImportError:
-    log.error("failed to import %s and/or %s",
-              "INeutronImplementationPlugin",
-              "BaseNeutronImplementationPlugin")
-    openstack = False
+from ZenPacks.zenoss.OpenStackInfrastructure.interfaces \
+    import INeutronImplementationPlugin
+from ZenPacks.zenoss.OpenStackInfrastructure.neutron_integration \
+    import BaseNeutronImplementationPlugin
 
-if openstack:
-    class OpenvSwitchNeutronImplementationPlugin(BaseNeutronImplementationPlugin):
-        implements(INeutronImplementationPlugin)
 
-        def getPortIntegrationKeys(self, osi_port):
-            # Use short name from OSI port ID as a clue to find the port on
-            # OVS side. Use that OVS port to determine the OVS host IP
-            short_osi_port_id = osi_port.id[5:16]
+class OpenvSwitchNeutronImplementationPlugin(BaseNeutronImplementationPlugin):
+    implements(INeutronImplementationPlugin)
 
-            # Get a list of all IPs of all hosts in the openstack environment
-            manageIps = set()
-            for host in osi_port.endpoint().getDeviceComponents(type='OpenStackInfrastructureHost'):
-                manageIps.add(host.proxy_device().manageIp)
+    def getPortIntegrationKeys(self, osi_port):
+        # Use short name from OSI port ID as a clue to find the port on
+        # OVS side. Use that OVS port to determine the OVS host IP
+        short_osi_port_id = osi_port.id[5:16]
 
-            keys = []
-            for manageIp in manageIps:
-                keyvalues = (manageIp, 'port', short_osi_port_id)
+        # Get a list of all IPs of all hosts in the openstack environment
+        manageIps = set()
+        for host in osi_port.endpoint().getDeviceComponents(type='OpenStackInfrastructureHost'):
+            manageIps.add(host.proxy_device().manageIp)
+
+        keys = []
+        for manageIp in manageIps:
+            keyvalues = (manageIp, 'port', short_osi_port_id)
+            keys.append('ml2.openvswitch:' + '|'.join(keyvalues))
+
+            if hasattr(osi_port, 'mac_address') and osi_port.mac_address:
+                keyvalues = (manageIp, 'interface', osi_port.mac_address)
                 keys.append('ml2.openvswitch:' + '|'.join(keyvalues))
 
-                if hasattr(osi_port, 'mac_address') and osi_port.mac_address:
-                    keyvalues = (manageIp, 'interface', osi_port.mac_address)
-                    keys.append('ml2.openvswitch:' + '|'.join(keyvalues))
+        return keys
 
-            return keys
+    @classmethod
+    def reindex_implementation_components(cls, dmd):
+        device_class = dmd.Devices.getOrganizer('/Network/OpenvSwitch')
+        results = ICatalogTool(device_class).search(
+            ('ZenPacks.zenoss.OpenvSwitch.Port.Port',
+                'ZenPacks.zenoss.OpenvSwitch.Interface.Interface',)
+        )
 
-        @classmethod
-        def reindex_implementation_components(cls, dmd):
-            device_class = dmd.Devices.getOrganizer('/Network/OpenvSwitch')
-            results = ICatalogTool(device_class).search(
-                ('ZenPacks.zenoss.OpenvSwitch.Port.Port',
-                 'ZenPacks.zenoss.OpenvSwitch.Interface.Interface',)
-            )
-
-            for brain in results:
-                obj = brain.getObject()
-                obj.index_object()
-                notify(IndexingEvent(obj))
+        for brain in results:
+            obj = brain.getObject()
+            obj.index_object()
+            notify(IndexingEvent(obj))
 
 
 def onOpenStackHostAdded(obj, event):
